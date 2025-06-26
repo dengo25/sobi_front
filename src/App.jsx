60cf5997ce602837f97fe378918ebdb6073d0217
dengo25 <dengo25.dev@gmail.com>
2025-06-26 20:46:03 +0900

    [Feat] 환경설정

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-3xl bg-amber-50 border-1 m-3'>
+        Hello Sobi Final
+      </div>
+    </>
+  )
+}
+
+export default App
