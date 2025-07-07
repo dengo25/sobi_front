8be39891551e76791060fec70ffcfd0529b07963
dengo25 <dengo25.dev@gmail.com>
2025-07-07 15:37:10 +0900

    [Feat] redux-persist 추가 및 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import router from "./router/root";
 import { RouterProvider } from "react-router-dom";
 import "./assets/styles/style.css";
 import './index.css'
 import {Provider} from "react-redux";
-import store from "./store.jsx";
+import store, {persistor} from "./store.jsx";
+import {PersistGate} from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
     <Provider store={store}>
-  <RouterProvider router={router} />
+        <PersistGate loading={null} persistor={persistor}>
+            <RouterProvider router={router} />
+        </PersistGate>
     </Provider>
 );
--- src/store.jsx
+++ src/store.jsx
@@ -1,10 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
-import memberSlice from "./slice/memberSlice";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import memberReducer from "./slice/memberSlice";
+
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // 기본: localStorage 사용
+
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: ["member"], // persist할 slice 이름
+};
 
 
-export default configureStore({
-    reducer: {
-        "memberSlice": memberSlice
-    },
+const rootReducer = combineReducers({
+    member: memberReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false, // 필수 설정
+        }),
+});
+
+export const persistor = persistStore(store);
+
+export default store;
