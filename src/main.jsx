521696361dda51646e7893df09b0d473660bb839
dengo25 <dengo25.dev@gmail.com>
2025-07-07 12:04:31 +0900

    [Feat] Redux && 로그인 전역상태 관리

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,11 @@ import router from "./router/root";
 import { RouterProvider } from "react-router-dom";
 import "./assets/styles/style.css";
 import './index.css'
+import {Provider} from "react-redux";
+import store from "./store.jsx";
 
 createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
   <RouterProvider router={router} />
+    </Provider>
 );
