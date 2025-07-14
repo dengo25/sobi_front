f0ae5883204846e3dfac4c0be9125f8186f8151a
dengo25 <dengo25.dev@gmail.com>
2025-07-14 22:16:16 +0900

    [Feat] backendHost 변경

--- src/service/util/api-config.js
+++ src/service/util/api-config.js
@@ -7,7 +7,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === "localhost") {
     backendHost = "http://localhost:8080";
 } else {
-    backendHost = "https://api.thekosta.com";
+    backendHost = "https://sobi.thekosta.com";
 }
 
 
